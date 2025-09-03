a0b0b99a99edff000deada6cd5a2d112eed19ad3
khoaadmt <khoaadmt@gmail.com>
2025-09-03 10:50:26 +0700

    feat(pay-standard-sim-page): continue implementing UI and responsive in mobile

--- src/pages/pay-standard-sim-page/PayStandardSimPage.tsx
+++ src/pages/pay-standard-sim-page/PayStandardSimPage.tsx
@@ -5,9 +5,14 @@ import { TfiReload } from "react-icons/tfi";
 import { Checkbox, Col, GetProp, Radio, Row, Segmented, Select } from "antd";
 import { Input } from "antd";
 import { ArrowRightOutlined, SearchOutlined } from "@ant-design/icons";
+import { FaPhoneAlt } from "react-icons/fa";
 import SelectWithLabel from "../../components/SelectWithLabel/SelectWithLabel";
-import "./PayStandardSimPage.css";
+import Logo from "../../assets/ITS - logo.png";
+import { LuPhoneCall } from "react-icons/lu";
+import { GrMapLocation } from "react-icons/gr";
+import { LuCalendarDays } from "react-icons/lu";
 
+import "./PayStandardSimPage.css";
 const { Search } = Input;
 
 export const PayStandardSimPage: React.FC = () => {
@@ -28,9 +33,9 @@ export const PayStandardSimPage: React.FC = () => {
     ];
     return (
         <div className="top-up-card-container mx-5 lg:mx-[6%] 2xl:mx-[15%]">
-            <div className="flex flex-col rounded-[20px] bg-white p-4 md:flex-row lg:p-8 xl:px-12 xl:pb-12">
+            <div className="flex flex-col rounded-[20px] bg-white p-4 md:flex-row md:items-start lg:p-8 xl:px-12 xl:pb-12">
                 <div className="box-content md:w-[55%] md:pr-4 lg:w-3/5 lg:pr-8 xl:pr-10">
-                    <h3 className="py-4 text-center text-xl font-bold lg:py-6 xl:text-2xl">
+                    <h3 className="py-4 text-center text-2xl font-bold lg:py-6 xl:text-3xl">
                         Confirm and pay
                     </h3>
                     <div className="sim-toggle-wrapper w-full">
@@ -42,7 +47,7 @@ export const PayStandardSimPage: React.FC = () => {
                             className="sim-segmented w-fu"
                         />
                     </div>
-                    <p className="py-4 text-lg font-bold">Recipient information</p>
+                    <p className="py-4 text-xl font-bold xl:text-2xl">Recipient information</p>
                     <div className="">
                         <PhoneInput
                             className="py-2"
@@ -63,7 +68,7 @@ export const PayStandardSimPage: React.FC = () => {
                             placeholder="Enter your first and last name"
                         ></PhoneInput>
                     </div>
-                    <p className="py-4 text-lg font-bold">Recipient information</p>
+                    <p className="py-4 text-xl font-bold xl:text-2xl">How to receive SIM</p>
                     <Checkbox.Group
                         style={{ width: "100%" }}
                         defaultValue={["Apple"]}
@@ -109,8 +114,97 @@ export const PayStandardSimPage: React.FC = () => {
                             <SelectWithLabel label="District" options={options} />
                         </div>
                     </div>
+                    <div className="mt-6 flex flex-col gap-6">
+                        <div className="border-primary flex flex-row justify-between rounded-[32px] p-6">
+                            <div className="flex flex-row gap-6">
+                                <img className="h-[40px] w-[60px]" src={Logo} alt="ITS Logo" />
+                                <div className="flex flex-col gap-1 text-sm">
+                                    <div>
+                                        <p className="pb-2 text-lg font-bold xl:text-xl">
+                                            ĐGD ITS Ha Noi
+                                        </p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <LuPhoneCall />
+                                        <p>0961 793 153</p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <GrMapLocation />
+                                        <p>Ha Noi</p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <LuCalendarDays />
+                                        <p>0h800 - 18h00</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <button className="border-primary rounded-md px-6 py-2 text-[16px]">
+                                    Select
+                                </button>
+                            </div>
+                        </div>
+                        <div className="border-primary flex flex-row justify-between rounded-[32px] p-6">
+                            <div className="flex flex-row gap-6">
+                                <img className="h-[40px] w-[60px]" src={Logo} alt="ITS Logo" />
+                                <div className="flex flex-col gap-1 text-sm">
+                                    <div>
+                                        <p className="pb-2 text-lg font-bold xl:text-xl">
+                                            ĐGD ITS Ha Noi
+                                        </p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <LuPhoneCall />
+                                        <p>0961 793 153</p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <GrMapLocation />
+                                        <p>Ha Noi</p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <LuCalendarDays />
+                                        <p>0h800 - 18h00</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <button className="border-primary rounded-md px-6 py-2 text-[16px]">
+                                    Select
+                                </button>
+                            </div>
+                        </div>
+                        <div className="border-primary flex flex-row justify-between rounded-[32px] p-6">
+                            <div className="flex flex-row gap-6">
+                                <img className="h-[40px] w-[60px]" src={Logo} alt="ITS Logo" />
+                                <div className="flex flex-col gap-1 text-sm">
+                                    <div>
+                                        <p className="pb-2 text-lg font-bold xl:text-xl">
+                                            ĐGD ITS Ha Noi
+                                        </p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <LuPhoneCall />
+                                        <p>0961 793 153</p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <GrMapLocation />
+                                        <p>Ha Noi</p>
+                                    </div>
+                                    <div className="text-tertiary item-center flex flex-row items-center gap-1">
+                                        <LuCalendarDays />
+                                        <p>0h800 - 18h00</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <button className="border-primary rounded-md px-6 py-2 text-[16px]">
+                                    Select
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="price-details border-primary mt-4 flex flex-col md:mt-0 md:w-[45%] lg:w-2/5">
+                <div className="price-details border-primary mt-4 flex flex-col rounded-2xl md:mt-0 md:w-[45%] lg:w-2/5">
                     <div className="border-primary m-6 rounded-lg border p-4 md:p-4 lg:m-7 lg:p-6">
                         <div className="flex flex-row items-center justify-between">
                             <div className="flex flex-row items-stretch gap-2">
@@ -142,7 +236,7 @@ export const PayStandardSimPage: React.FC = () => {
                         </div>
                     </div>
                     <div className="mx-4 flex flex-col gap-2 lg:mx-6 lg:mb-6">
-                        <h3 className="text-xl font-bold lg:text-2xl">Price details</h3>
+                        <h3 className="text-xl font-bold xl:text-2xl">Price details</h3>
                         <div className="basi-[30%] flex h-full flex-col justify-between">
                             <div className="mx-1 flex flex-row justify-between py-2 xl:mx-2">
                                 <p>Number of subscribers</p>
